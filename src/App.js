5efc13257c0063ebd0019dab4a279d47cf6078b5
u00t0 <sato0uta0@gmail.com>
2020-10-10 15:21:04 +0900

    ラウンティングできた

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import Top from "./screens/Top"
+import NavBar from "./components/NavBar"
+import Quiz from './screens/Quiz';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App">aaaaaa</div>
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <div className="container">
+        <Switch>
+          <Route exact path="/" component={Top} />
+          <Route path="/quiz" component={Quiz} />
+          <Route render={() => <h4>not found...</h4>} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+
 }
 
 export default App;
